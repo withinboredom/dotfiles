43938c4d92cd4eb6f68697570c69a46eb111393c
Rob Landers <landers.robert@gmail.com>
2016-12-27 21:13:18 -0500

    Initial commit

---
+++ configure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -v
+
+sudo apt update
+sudo apt -y upgrade
+
+sudo apt install -y \
+	curl wget gnupg2 python-pip python3-pip terminator zsh build-essential jq
+
+curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
+
+sudo apt upgrade -y
+sudo apt install -y nodejs
+
+sudo npm install -g npm yarn
+
+curl -sL https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-add-repository ppa:git-core/ppa
+
+sudo apt-get install git-lfs git
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo apt update
+sudo apt upgrade -y
+
+set +e
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Dotfiles for bash for windows
+
+1. Simply start a bash for windows session
+1. download and execute the configure.sh script
+1. go to your home directory
+1. git clone this directory to your home directory (and optionally delete the .git folder)
+1. open a new shell and you'll be started up in zsh
